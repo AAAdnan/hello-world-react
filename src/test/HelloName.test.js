b897f2b2732f626931ddf2e14de44c1c82346442
Adnan Ahmed <adnan.ahmed01@gmail.com>
2020-01-10 21:58:21 +0000

    added react library testing for HelloName

--- src/test/HelloName.test.js
+++ src/test/HelloName.test.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import {render, fireEvent, wait, cleanup} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import UserContext from '../UserContext';
 import HelloName from '../components/HelloName';
 
 afterEach(cleanup)
 
-describe('This will test HelloName Component', () => {
+function renderHelloName(user) {
+    return render(
+        <UserContext.Provider value={user}>
+            <HelloName />
+        </UserContext.Provider>
+    );
+}
+
+describe('This will test HelloName Component with context', () => {
+    it('Has element with a question if it has no name', () => {
+        const { getByText } = renderHelloName('');
+        expect(getByText('Hello, What is your name?')).toBeInTheDocument();
+    })
+
     it('Has element with text Hello {name}', () => {
-        const { getByText } = render(<HelloName name="Adnan" />)
+        const user = { name: 'Adnan' }
+        const { getByText } = renderHelloName(user);
+        expect(getByText(`Hello, ${user.name}`)).toBeInTheDocument();
+    });
 
-        expect(getByText('Hello, Adnan')).toBeInTheDocument()
-    })
-    it('Has element with a question if there is no name', () => {
-        const { getByText } = render(<HelloName />)
+    it('Has element with text Hello {name}', () => {
+        const user = { name: 'Jim' }
+        const { getByText } = renderHelloName(user);
+        expect(getByText(`Hello, ${user.name}`)).toBeInTheDocument();
+    });
 
-        expect(getByText('Hello, What is your name?')).toBeInTheDocument()
-    })
-})
+});
 
